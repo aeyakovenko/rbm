7c27cbaa042c6cc40714b56b462196fd0f5b6ce3
anatoly yakovenko <aeyakovenko@gmail.com>
2015-03-01 17:01:34 -0800

    update

--- RBM.hs
+++ RBM.hs
@@ -7,16 +7,14 @@ module RBM(rbm
 
 --benchmark modules
 import Criterion.Main(defaultMainWith,defaultConfig,bgroup,bench,whnf)
-import Criterion.Types(reportFile)
+import Criterion.Types(reportFile,timeLimit)
 --test modules
 import System.Exit (exitFailure)
-import Test.QuickCheck(verboseCheckResult)
-import Test.QuickCheck.Test(isSuccess)
+import Test.QuickCheck(verboseCheckWithResult)
+import Test.QuickCheck.Test(isSuccess,stdArgs,maxSuccess)
 
---import Data.List.Stream(map,zipWith,take,sum,take,drop,concat,repeat,(!!),replicate,length)
---import qualified Prelude()
---import Prelude(($),Double,Int,IO,negate,fromIntegral,return,(*),(/),(+),exp,negate,(.),fst,flip,(<),isNaN,otherwise,(-),Bool,not,(==),(=<<),Maybe(Just))
 import Data.Word(Word8)
+import Data.List(transpose)
 import Control.Monad.State.Lazy(runState
                                ,get
                                ,State
@@ -34,8 +32,10 @@ data RBM = RBM { weights :: [Double] -- weight matrix, numHidden + 1 x numInputs
                , numHidden :: Int
                }
 
-rbm :: Int -> Int -> RBM
-rbm ni nh = RBM [] ni nh
+rbm :: RandomGen r => r -> Int -> Int -> RBM
+rbm r ni nh = RBM nw ni nh
+   where
+      nw = take ((nh + 1)* (ni + 1)) $ randomRs (0,1) r
 
 energy :: RBM -> [Double] -> Double
 energy rb inputs = negate ee
@@ -53,27 +53,21 @@ groupByN _ [] = []
 groupByN n ls = (take n ls) : groupByN n (drop n ls)
 
 hiddenProbs :: RBM -> [Double] -> [Double]
-hiddenProbs rb biased = map (sigmoid . sum) $ groupByN (ni + 1) $ zipWith (*) wws $ concat $ repeat biased 
+hiddenProbs rb biased = map (sigmoid . sum) $ groupByN ni $ zipWith (*) wws $ cycle biased 
    where
       wws = weights rb
-      ni = numInputs rb
+      ni = (numInputs rb) + 1
 
 inputProbs :: RBM -> [Double] -> [Double]
-inputProbs rb hidden = [ sigmoid (sums (weights rb) ii) | ii <- [0..ni] ] 
+inputProbs rb hidden = map (sigmoid . sum) $ groupByN nh $ zipWith (*) wws hhs
    where
-      ni = numInputs rb
-      nh = numHidden rb
-      sums wws ii = sum [ (wws !! (ii * nh + jj)) * (hidden !! ii) | jj <- [0..nh] ]
-
-initWeights :: RandomGen r => RBM -> State r RBM
-initWeights (RBM [] ni nh) = do
-   nw <- take ((nh + 1)* (ni + 1)) <$> randomRs (0,1) <$> getR
-   return $ RBM nw ni nh
-initWeights rb = return $ rb   
+      hhs = concat $ transpose $ replicate ni hidden
+      wws = weights rb
+      ni = (numInputs rb) + 1
+      nh = (numHidden rb) + 1
 
 sample :: RandomGen r => r -> RBM -> [Double] -> RBM
-sample rand rb' inputs = fst $ (flip runState) rand $ do
-   rb <- initWeights rb'
+sample rand rb inputs = fst $ (flip runState) rand $ do
    let nh = numHidden rb
    hrands <- take nh <$> (:) 1 <$> randomRs (0,1) <$> getR
    let biased = 1:inputs
@@ -92,9 +86,7 @@ sample rand rb' inputs = fst $ (flip runState) rand $ do
    return $ rb { weights = uw }
 
 vmult :: [Double] -> [Double] -> [Double]
-vmult inputs hidden = concat [ line xx | xx <- inputs]
-   where
-      line xx = [ (xx * yy) | yy <- hidden] 
+vmult inputs hidden = [ (xx*yy) | xx <- inputs, yy<-hidden]
 
 getR :: RandomGen r => State r r
 getR = do
@@ -103,34 +95,57 @@ getR = do
    return $ rd
 
 prop_init :: Int -> Word8 -> Word8 -> Bool
-prop_init gen ni nh = ((fi ni) + 1) * ((fi nh) + 1) == (length $ weights $ fst $ runst $ doinit)
+prop_init gen ni nh = ((fi ni) + 1) * ((fi nh) + 1) == (length $ weights rb)
    where
-      runst = (flip runState) (mkStdGen gen)
-      doinit = initWeights $ rbm (fi ni) (fi nh)
+      rb = rbm (mkStdGen gen) (fi ni) (fi nh)
+      fi = fromIntegral
+
+prop_hiddenProbs :: Int -> Word8 -> Word8 -> Bool
+prop_hiddenProbs gen ni nh = (fi nh) + 1 == length pp
+   where
+      pp = hiddenProbs rb $ replicate ((fi ni) + 1) 0.0
+      rb = rbm (mkStdGen gen) (fi ni) (fi nh)
+      fi = fromIntegral
+
+prop_inputProbs :: Int -> Word8 -> Word8 -> Bool
+prop_inputProbs gen ni nh = (fi ni) + 1 == length pp
+   where
+      pp = inputProbs rb $ replicate ((fi nh) + 1) 0.0
+      rb = rbm (mkStdGen gen) (fi ni) (fi nh)
       fi = fromIntegral
 
 prop_energy :: Int -> Word8 -> Word8 -> Bool
 prop_energy gen ni nh = not $ isNaN ee
    where
       ee = energy rb $ replicate (fi ni) 0.0
-      rb = fst $ runst $ doinit
-      runst = (flip runState) (mkStdGen gen)
-      doinit = initWeights $ rbm (fi ni) (fi nh)
+      rb = rbm (mkStdGen gen) (fi ni) (fi nh)
       fi = fromIntegral
 
 test :: IO ()
 test = do
    let check rr = if (isSuccess rr) then return () else exitFailure
-       runtest p = check =<< verboseCheckResult p 
+       cfg = stdArgs { maxSuccess = 50 } 
+       runtest p = check =<< verboseCheckWithResult cfg p 
    runtest prop_init
    runtest prop_energy
+   runtest prop_hiddenProbs
+   runtest prop_inputProbs
 
 perf :: IO ()
-perf = defaultMainWith (defaultConfig { reportFile = Just "dist/perf-RBM.html" }) [ 
-   bgroup "energy" [ bench "3x3"  $ whnf (prop_energy 0 3) 3
-                   , bench "31x31"  $ whnf (prop_energy 0 31) 31
-                   , bench "63x63"  $ whnf (prop_energy 0 63) 63
-                   , bench "127x127"  $ whnf (prop_energy 0 127) 127
-                   , bench "255x255"  $ whnf (prop_energy 0 255) 255
-                   ]
-   ]
+perf = do
+   let cfg = defaultConfig { reportFile = Just "dist/perf-RBM.html", timeLimit = 0.5 }
+   defaultMainWith cfg [
+       bgroup "energy" [ bench "3x3"  $ whnf (prop_energy 0 3) 3
+                       , bench "127x127"  $ whnf (prop_energy 0 127) 127
+                       , bench "255x255"  $ whnf (prop_energy 0 255) 255
+                       ]
+      ,bgroup "hidden" [ bench "3x3"  $ whnf (prop_hiddenProbs 0 3) 3
+                       , bench "127x127"  $ whnf (prop_hiddenProbs 0 127) 127
+                       , bench "255x255"  $ whnf (prop_hiddenProbs 0 255) 255
+                       ]
+      ,bgroup "input" [ bench "3x3"  $ whnf (prop_inputProbs 0 3) 3
+                      , bench "127x127"  $ whnf (prop_inputProbs 0 127) 127
+                      , bench "255x255"  $ whnf (prop_inputProbs 0 255) 255
+                      ]
+      ]
+   putStrLn "perf log written to dist/perf-RBM.html"
