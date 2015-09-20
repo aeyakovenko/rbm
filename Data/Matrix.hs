71ee6c6f1df03f401b6e9f4cdff02b0f884680f6
anatoly yakovenko <aeyakovenko@gmail.com>
2015-09-20 15:57:14 -0700

    cleanup

--- Data/Matrix.hs
+++ Data/Matrix.hs
@@ -24,6 +24,8 @@ import Data.Array.Repa(Array
                       ,All(All)
                       )
 
+-- | wraps the Repa Array types so we can typecheck the results of
+-- | the matrix operations
 data Matrix d a b = R.Source d Double => Matrix (Array d DIM2 Double)
 
 instance NFData (Matrix U a b) where
@@ -31,71 +33,90 @@ instance NFData (Matrix U a b) where
 
 class MatrixOps a b where
    mmult :: Monad m => (Matrix U a b) -> (Matrix U b c) -> m (Matrix U a c)
+   mmult (Matrix ab) (Matrix ba) = Matrix <$> (ab `mmultP` ba)
+
    mmultT :: Monad m => (Matrix U a b) -> (Matrix U c b) -> m (Matrix U a c)
+   mmultT (Matrix ab) (Matrix ab') = Matrix <$> (ab `mmultTP` ab')
+
    d2u :: Monad m => Matrix D a b -> m (Matrix U a b)
+   d2u (Matrix ar) = Matrix <$> (R.computeP ar)
+
    (*^) :: Matrix c a b -> Matrix d a b -> (Matrix D a b)
+   (Matrix ab) *^ (Matrix ab') = Matrix (ab R.*^ ab')
+   {-# INLINE (*^) #-}
+
    (+^) :: Matrix c a b -> Matrix d a b -> (Matrix D a b)
+   (Matrix ab) +^ (Matrix ab') = Matrix (ab R.+^ ab')
+   {-# INLINE (+^) #-}
+
    (-^) :: Matrix c a b -> Matrix d a b -> (Matrix D a b)
+   (Matrix ab) -^ (Matrix ab') = Matrix (ab R.-^ ab')
+   {-# INLINE (-^) #-}
+
    map :: (Double -> Double) -> Matrix c a b -> (Matrix D a b)
+   map f (Matrix ar) = Matrix (R.map f ar)
+   {-# INLINE map #-}
+
    cast1 :: Matrix c a b -> Matrix c d b
+   cast1 (Matrix ar) = Matrix ar
+   {-# INLINE cast1 #-}
+
    cast2 :: Matrix c a b -> Matrix c a d
+   cast2 (Matrix ar) = Matrix ar
+   {-# INLINE cast2 #-}
+
    transpose :: Monad m => Matrix U a b -> m (Matrix U b a)
+   transpose (Matrix ar) = Matrix <$> (R.transpose2P ar)
+
    sum :: Monad m =>  Matrix c a b -> m Double
+   sum (Matrix ar) = R.sumAllP ar
+
    elems :: Matrix c a b -> Int
    elems m = (row m) * (col m)
+
    {-# INLINE elems #-}
    row :: Matrix c a b -> Int
+   row (Matrix ar) = (R.row (R.extent ar))
+   {-# INLINE row #-}
+
    col :: Matrix c a b -> Int
+   col (Matrix ar) = (R.col (R.extent ar))
+   {-# INLINE col #-}
+
    shape :: Matrix c a b -> (Int,Int)
    shape m = (row m, col m)
    {-# INLINE shape #-}
-   randomish :: (Int,Int) -> (Double,Double) -> Int -> Matrix U a b
-   extractRows :: (Int,Int) -> Matrix c a b -> Matrix D a b 
-   zipWith :: (Double -> Double -> Double) -> Matrix c a b -> Matrix c a b -> (Matrix D a b)
-   fromList :: (Int,Int) -> [Double] -> Matrix U a b
-   traverse :: (Double -> Int -> Int -> Double) -> Matrix c a b -> Matrix D a b
-   toList :: Matrix U a b -> [Double]
 
-instance MatrixOps a b where
-   mmult (Matrix ab) (Matrix ba) = Matrix <$> (ab `mmultP` ba)
-   mmultT (Matrix ab) (Matrix ab') = Matrix <$> (ab `mmultTP` ab')
-   d2u (Matrix ar) = Matrix <$> (R.computeP ar)
-   (Matrix ab) *^ (Matrix ab') = Matrix (ab R.*^ ab')
-   {-# INLINE (*^) #-}
-   (Matrix ab) +^ (Matrix ab') = Matrix (ab R.+^ ab')
-   {-# INLINE (+^) #-}
-   (Matrix ab) -^ (Matrix ab') = Matrix (ab R.-^ ab')
-   {-# INLINE (-^) #-}
-   map f (Matrix ar) = Matrix (R.map f ar)
-   {-# INLINE map #-}
-   cast1 (Matrix ar) = Matrix ar
-   {-# INLINE cast1 #-}
-   cast2 (Matrix ar) = Matrix ar
-   {-# INLINE cast2 #-}
-   transpose (Matrix ar) = Matrix <$> (R.transpose2P ar)
-   sum (Matrix ar) = R.sumAllP ar
-   row (Matrix ar) = (R.row (R.extent ar))
-   {-# INLINE row #-}
-   col (Matrix ar) = (R.col (R.extent ar))
-   {-# INLINE col #-}
+   randomish :: (Int,Int) -> (Double,Double) -> Int -> Matrix U a b
    randomish (r,c) (minv,maxv) seed = Matrix $ R.randomishDoubleArray (Z :. r :. c) minv maxv seed
    {-# INLINE randomish #-}
+
+   extractRows :: (Int,Int) -> Matrix c a b -> Matrix D a b 
    extractRows (rix,num) mm@(Matrix ar) = Matrix $ R.extract
                                           (Z :. rix :. 0)
                                           (Z :. num :. (col mm))
                                           ar
    {-# INLINE extractRows #-}
 
+
+   zipWith :: (Double -> Double -> Double) -> Matrix c a b -> Matrix c a b -> (Matrix D a b)
    zipWith f (Matrix aa) (Matrix bb) = Matrix (R.zipWith f aa bb)
    {-# INLINE zipWith #-}
+
+   fromList :: (Int,Int) -> [Double] -> Matrix U a b
    fromList (r,c) lst = Matrix $ R.fromListUnboxed (Z:.r:.c) lst
    {-# INLINE fromList #-}
+
+   traverse :: (Double -> Int -> Int -> Double) -> Matrix c a b -> Matrix D a b
    traverse ff (Matrix ar) = Matrix $ R.traverse ar id func
          where func gv sh@(Z :. rr :. cc) = ff (gv sh) rr cc
    {-# INLINE traverse #-}
+
+   toList :: Matrix U a b -> [Double]
    toList (Matrix ab) = R.toList ab
    {-# INLINE toList #-}
 
+instance MatrixOps a b where
 
 {--
  - matrix multiply
